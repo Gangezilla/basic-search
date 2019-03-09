0e93ca7715807b8934c2b2de3efb2b5214938f9f
Scott <scottgangemi@gmail.com>
2019-03-09 14:44:43 +1100

    Generate indexes, cool

--- inverted-index/index.js
+++ inverted-index/index.js
@@ -1,28 +1,48 @@
-// const stemmer = require("./stemmer");
-// const tokenizer = require("./tokenizer");
-const Snowball = require("snowball");
 const fs = require("fs");
 const path = require("path");
+const natural = require("natural");
 
-const stemmer = new Snowball("English");
-// stemmer.setCurrent("guidance hello boys");
-// stemmer.stem();
-// console.log(stemmer.getCurrent());
+const calculateTokenFrequency = tokens => {
+  const unsortedTokens = tokens.reduce((count, word) => {
+    count[word] = (count[word] || 0) + 1;
+    return count;
+  }, {});
+
+  const sortedTokens = {};
+  Object.keys(unsortedTokens)
+    .sort()
+    .forEach(key => (sortedTokens[key] = unsortedTokens[key]));
+  return sortedTokens;
+};
+
+const normaliseText = text =>
+  text.toLowerCase().replace(/[.,\/#!?$%\^&\*;:\[\]{}=\-_`~()"']/gm, "");
 
 const processDocuments = () => {
   const dir = path.join(__dirname + "/../documents");
   fs.readdir(dir, (err, files) => {
-    files.forEach(file => {
-      const content = JSON.parse(fs.readFileSync(dir + "/" + file));
-      stemmer.setCurrent(content.text);
-      // stemmer.setCurrent("Creeps in this petty pace day by day");
-      stemmer.stem();
-      // console.log(stemmer.getCurrent());
-    });
+    try {
+      files.forEach(file => {
+        const newPath = path.join(
+          __dirname + `/../indexes/${file.split(".")[0]}.index`
+        );
+        natural.PorterStemmer.attach();
+        natural.LancasterStemmer.attach();
+
+        const content = JSON.parse(fs.readFileSync(dir + "/" + file));
+        const normalisedText = normaliseText(content.text);
+        const stemmedText = normalisedText.tokenizeAndStem();
+        const index = calculateTokenFrequency(stemmedText);
+        fs.writeFile(newPath, JSON.stringify(index, null, 2), (err, _data) => {
+          if (err) throw err;
+        });
+      });
+    } catch (err) {
+      console.error("Something went wrong sorry!", err);
+    }
   });
 };
 
 module.exports = processDocuments;
 
-// read file in from FS, run snowball
-// generate a new inverted index JSON file.
+// // fs.appendFileSync("./formatted-styles", styles);
