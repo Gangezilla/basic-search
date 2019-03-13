d14cd086a32fa2b90f0dde985543ca570d80342c
Scott <scottgangemi@gmail.com>
2019-03-13 21:13:23 +1100

    Last step, need to figure out how to find first instance of phrase and surrounding words

--- src/queryController.js
+++ src/queryController.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
+const { readFileAsync } = require("./helpers");
 
 const findPostings = query => {
   // O(n^2) but its ok for now
   // plus i get this gut feeling that accessing the global object is bad
   // but its probably less bad than doing a file read every query.
-  const invertedIndex = JSON.parse(global.invertedIndex);
   const parsedQuery = query.toLowerCase().split(" ");
+  const invertedIndex = JSON.parse(global.invertedIndex);
   let tempIndex = {};
   Object.keys(invertedIndex).forEach(term => {
     parsedQuery.forEach(queryTerm => {
@@ -20,20 +21,18 @@ const findPostings = query => {
 };
 
 const findCommonPostings = postings => {
-  // finding all occurences of each matched word
-  const allPostings = Object.keys(postings).map(
-    term => postings[term].postings_list
-  );
+  // finding all occurences of each matched word and flattening
+  const flattenedPostings = Object.keys(postings)
+    .map(term => postings[term].postings_list)
+    .flat(1);
 
-  // flattening the array and counting the number of occurences
-  const flattenedPostings = [].concat.apply([], allPostings);
   const count = {};
   flattenedPostings.forEach(num => {
     count[num] = count[num] ? count[num] + 1 : 1;
   });
 
   // find elements that appear the same number of times as the input arrays,
-  // this will show us if the term has appeared in all the documents documents
+  // this will show us if the term has appeared in all the documents
   const numberOfMatchingDocuments = Object.keys(postings).length;
   const results = Object.keys(count).filter(
     num => count[num] >= numberOfMatchingDocuments
@@ -42,26 +41,38 @@ const findCommonPostings = postings => {
   return results;
 };
 
-const getTermsInDocument = documents => {
-  let termIndex = {};
-  documents.forEach(doc => {
-    fs.readFile(doc, "utf-8", (err, data) => {
-      const content = JSON.parse(data);
-      // console.log(content);
-    });
-  });
+const getTermsInDocument = (filenames, queryTerms) => {
+  Promise.all(filenames.map(filename => readFileAsync(filename))).then(
+    files => {
+      files.forEach(file => {
+        const parsedFile = JSON.parse(file);
+        const text = parsedFile.text;
+        // console.log(text);
+        // find word(s) in text, get words around them.
+        const splitText = text.toLowerCase().split(" ");
+        // splitText.forEach(docTerm => {
+        //   queryTerms.forEach(term => {
+        //     if (docTerm === term) {
+        //       console.log("!!!!!!!!!!");
+        //     }
+        //   });
+        // });
+        console.log(splitText);
+      });
+    }
+  );
 };
 
 const handleQuery = (req, res) => {
   const query = req.body.query;
   const postings = findPostings(query);
   const commonPostings = findCommonPostings(postings);
-  console.log(commonPostings);
-  const documents = commonPostings.map(
+  const matchedPhrases = Object.keys(postings);
+  const filenames = commonPostings.map(
     num => global.documentIndex[num].filename
   );
-  console.log(documents);
-  getTermsInDocument(documents);
+  getTermsInDocument(filenames, matchedPhrases);
+  // want to also get non-matched phrases and chuck them back too.
 
   res.sendStatus(200);
 };
